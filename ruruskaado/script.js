db2590affe5686c3cca7f78b82a5a0ed99da4aef
ruruskaado <95123918+ruruskaado@users.noreply.github.com>
2021-12-19 23:33:27 +0700

    Add files via upload

---
+++ ruruskaado/script.js
@@ -0,0 +1,7 @@
+document.getElementById('pfp1').focus();
+
+//  const menuSelection = document.getElementsByClassName(pfp);
+
+//  menuSelection.addEventListener('focus', (event) => {
+//      event.target.style
+//  })
